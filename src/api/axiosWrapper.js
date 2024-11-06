a3eb0c345e85bd983d73f6059a91a87114e9deb8
kwang9705 <kwang9705@gmail.com>
2024-11-06 15:05:35 +0900

    [FE] feat: month, year Statistics

--- src/api/axiosWrapper.js
+++ src/api/axiosWrapper.js
@@ -31,31 +31,31 @@ const createAxiosInstance = (baseURL) => {
     }
   );
 
-  //   // 응답 인터셉터 설정
-  //   instance.interceptors.response.use(
-  //     (response) => {
-  //       return response;
-  //     },
-  //     (error) => {
-  //       // 401 에러 처리: 세션 종료 및 로그아웃 처리
-  //       if (error.response?.status === 401 || error.response?.status === 403) {
-  //         handleLogout();
-  //       }
-  //       return Promise.reject(error);
-  //     }
-  //   );
+  // 응답 인터셉터 설정
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      // 401 에러 처리: 세션 종료 및 로그아웃 처리
+      if (error.response?.status === 401 || error.response?.status === 403) {
+        handleLogout();
+      }
+      return Promise.reject(error);
+    }
+  );
 
-  //   return instance;
-  // };
-  // // 로그아웃 처리 함수
-  // const handleLogout = () => {
-  //   const cookies = new Cookies();
-  //   cookies.remove("accessToken"); // 쿠키에서 accessToken 제거
-  //   localStorage.clear(); // 세션 스토리지 클리어
+  return instance;
+};
+// 로그아웃 처리 함수
+const handleLogout = () => {
+  const cookies = new Cookies();
+  cookies.remove("accessToken"); // 쿠키에서 accessToken 제거
+  localStorage.clear(); // 세션 스토리지 클리어
 
-  //   Swal.fire("세션이 만료되었습니다.", "다시 로그인 해주세요.", "info").then(() => {
-  //     window.location.reload();
-  //   });
+  Swal.fire("세션이 만료되었습니다.", "다시 로그인 해주세요.", "info").then(() => {
+    window.location.reload();
+  });
 };
 
 // 설정된 API URL을 사용하는 Axios 인스턴스
